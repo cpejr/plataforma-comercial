b389b15716cef8ea96b575ea2d7a366534082963
Marina Faria <maarina.faria@gmail.com>
2020-01-17 16:28:02 -0300

    .env bug fixed
    It needed REACT_APP_ before the variables to work

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,18 +1,19 @@
 import * as firebase from 'firebase/app'
+import 'firebase/auth'
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
-console.log(process.env.NODE_ENV);
-console.log(process.env.MONGO_USER);
-console.log(process.env.FIREBASE_API_KEY);
+console.log(process.env.REACT_APP_NODE_ENV);
+console.log(process.env.REACT_APP_MONGO_USER);
+console.log(process.env.REACT_APP_FIREBASE_API_KEY);
 console.log(process.env.REACT_APP_API_URL);
 
 const appFirebase = firebase.initializeApp({
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   });
 
   export default appFirebase;
